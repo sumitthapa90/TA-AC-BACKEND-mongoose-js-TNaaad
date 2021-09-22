b0593cd66ea2bb858314a75dcded4614101ac53d
sumit <sumitthapa90@gmail.com>
2021-09-22 14:14:08 +0530

    BNaacf again

--- block-BNaacf/app/model/user.js
+++ block-BNaacf/app/model/user.js
@@ -3,5 +3,9 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-    
-})
+  name: String,
+  email: { type: String, lowercase: true },
+  age: { type: Number, default: 0 },
+  favorites: [String],
+  marks: [Number],
+});
